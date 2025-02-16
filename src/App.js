31b6b3da9a25eb3118b544a617fcf1256063783e
navduhan <duhan27dec@gmail.com>
2025-02-15 23:52:58 -0600

    fixed static routing

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function Nav() {
         <Route path="/education" element={<Education/>} />
         <Route path="/publications" element={<Publications/>} />
         <Route path="/contact" element={<Contact/>} />
-        {/* <Route path="*" element={<Home/>} /> */}
+        <Route path="*" element={<Home/>} />
       </Routes>
     </Router>
   );
